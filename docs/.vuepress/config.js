37809071a32593a0d1380dad4a128ed74debfdc8
Shinemax <510360345@qq.com>
2018-11-06 12:57:23 +0800

    2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
       href: '/favicon.ico'
     }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  // base: '/web_accumulate/', // 这是部署到github相关的配置 下面会讲
+  base: '/dist/', // 这是部署到github相关的配置 下面会讲
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
